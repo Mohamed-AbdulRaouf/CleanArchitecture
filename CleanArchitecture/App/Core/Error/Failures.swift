cce5bbc699a1f956fe58fd8dbb9ab49668174c54
Mohamed Abdul-Raouf <mohamed.a.raouf@icloud.com>
2022-07-20 11:29:26 +0200

    add empty files Failures and Usecase

---
+++ CleanArchitecture/App/Core/Error/Failures.swift
@@ -0,0 +1,8 @@
+//
+//  Failures.swift
+//  CleanArchitecture
+//
+//  Created by mohamed on 7/20/22.
+//
+
+import Foundation
---
+++ CleanArchitecture/App/Core/Usecases/Usecase.swift
@@ -0,0 +1,8 @@
+//
+//  Usecase.swift
+//  CleanArchitecture
+//
+//  Created by mohamed on 7/20/22.
+//
+
+import Foundation
