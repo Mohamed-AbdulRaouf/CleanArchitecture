eaddfa0a5f964f6ac649599867eaedc8767db9c2
Mohamed Abdul-Raouf <mohamed.a.raouf@icloud.com>
2022-07-26 11:31:50 +0200

    update local data source with implementation

--- CleanArchitecture/App/Features/Number Trivia/Data/Datasources/NumberTriviaLocalDataSource.swift
+++ CleanArchitecture/App/Features/Number Trivia/Data/Datasources/NumberTriviaLocalDataSource.swift
@@ -9,8 +9,28 @@ import Foundation
 
 typealias NumberTriviaLocalDataSourceHandler = (NumberTriviaModel) -> ()
 
-protocol NumberTriviaLocalDataSource: AnyObject {
+protocol NumberTriviaLocalDataSourcePro: AnyObject {
     
     func getLastNumberTrivia(completion: NumberTriviaLocalDataSourceHandler)
     func cacheNumberTrivia(triviaToCache: NumberTriviaModel)
+}
+
+class NumberTriviaLocalDataSource: NumberTriviaLocalDataSourcePro {
+    
+    func getLastNumberTrivia(completion: (NumberTriviaModel) -> ()) {
+        if let savedPerson = UserDefaults.standard.object(forKey: "NumberTrivia") as? Data {
+            let decoder = JSONDecoder()
+            if let numberTrivia = try? decoder.decode(NumberTriviaModel.self, from: savedPerson) {
+                completion(numberTrivia)
+            }
+        }
+    }
+    
+    func cacheNumberTrivia(triviaToCache: NumberTriviaModel) {
+        let encoder = JSONEncoder()
+        if let encoded = try? encoder.encode(triviaToCache) {
+            let defaults = UserDefaults.standard
+            defaults.set(encoded, forKey: "NumberTrivia")
+        }
+    }
 }
